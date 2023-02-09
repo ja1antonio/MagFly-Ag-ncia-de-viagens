8a2baee1ebaa30402a6f2ef1eb6c6c7eadae9196
ja1antonio <ja1.negocios77@gmail.com>
2023-02-09 08:29:59 -0400

    Created Area Of Support

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
+//Imported images
+import grindImage from '../../assets/img_grid_ja1.png';
+
 const Support = () => {
-  return <div>Support</div>;
+  return (
+    <div className="support container section">
+      <div className="sectionContainer">
+        <div className="tittleDiv">
+          <small>travel support</small>
+          <h2>Plan your travel with confidence</h2>
+          <p>
+            Find help with booking and travel plans, see what to expect along
+            the journey!
+          </p>
+        </div>
+
+        <div className="infoDiv grid">
+          <div className="textDiv grid">
+            <div className="singleInfo">
+              <span className="number">01</span>
+              <h4>Travel requirements for Dubai</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+
+            <div className="singleInfo">
+              <span className="number">02</span>
+              <h4>Chaffeur services at your arrival</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+
+            <div className="singleInfo">
+              <span className="number">03</span>
+              <h4>Multi-risk travel insurance</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+          </div>
+          <div className="imgDiv">
+            <img src={grindImage} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Support;
