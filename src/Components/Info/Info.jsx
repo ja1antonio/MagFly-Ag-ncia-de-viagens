8b77a4120bb19f7fa4e2da90da72d69cfb8358e6
ja1antonio <ja1.negocios77@gmail.com>
2023-02-10 08:11:59 -0400

    Info Finalizado

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -26,7 +26,7 @@ const Info = () => {
             </p>
           </div>
           <div className="singleCard grid">
-            <div className="iconDiv flex">
+            <div className="iconDiv flex colorOne">
               <BsShieldCheck className="icon" />
             </div>
             <span className="cardTitle">Smart Checklist</span>
@@ -36,7 +36,7 @@ const Info = () => {
             </p>
           </div>
           <div className="singleCard grid">
-            <div className="iconDiv flex">
+            <div className="iconDiv flex colorTwo">
               <BsBookmarkCheck className="icon" />
             </div>
             <span className="cardTitle">Save More</span>
