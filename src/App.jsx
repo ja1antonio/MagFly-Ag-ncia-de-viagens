1b322c1c912894146404932dc00bce9f62045a05
ja1antonio <ja1.negocios77@gmail.com>
2023-02-11 15:28:50 -0400

    Começando footer

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
       <Lounge />
       <Travelers />
       <Subscribers />
-      {/*  <Footer /> */}
+      <Footer />
     </div>
   );
 };
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+//Imported Images
+import Logo from '../../assets/logo.png';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="sectionContainer container grid">
+        <div className="grindOne">
+          <div className="logoDiv">
+            <img src={Logo} className="Logo" />
+          </div>
+          <p>Your mind should be stronger than your feelings, fly</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
