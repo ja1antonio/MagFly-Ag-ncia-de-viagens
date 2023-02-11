826f0a443f5c27d3c15fb6f5a532ea80b3cadabf
ja1antonio <ja1.negocios77@gmail.com>
2023-02-10 23:47:58 -0400

    Component Loung Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ const App = () => {
       <Search />
       <Support />
       <Info />
-      {/* <Lounge />
-      <Travelers />
+      <Lounge />
+      {/* <Travelers />
       <Subscribers />
       <Footer /> */}
     </div>
